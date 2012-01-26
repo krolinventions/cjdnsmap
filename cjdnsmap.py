02bf7d789a355fb9c6b23e1a0c18ddbe01599021
Gerard Krol <g@gkrol.nl>
2012-01-26 20:53:50 +0100

    Add license for bencode parser.

--- cjdnsmap.py
+++ cjdnsmap.py
@@ -19,12 +19,34 @@
 #
 
 import pydot
-import os.path # for commonprefix
-
-##################
-## code from http://effbot.org/zone/bencode.htm
-
+import requests
 import re
+
+#################################################
+# code from http://effbot.org/zone/bencode.htm
+#
+# Copyright © 1995-2010 by Fredrik Lundh 
+#
+# By obtaining, using, and/ or copying this software and/or its 
+# associated documentation, you agree that you have read, understood, 
+# and will comply with the following terms and conditions:
+#
+# Permission to use, copy, modify, and distribute this software and its
+# associated documentation for any purpose and without fee is hereby 
+# granted, provided that the above copyright notice appears in all 
+# copies, and that both that copyright notice and this permission 
+# notice appear in supporting documentation, and that the name of 
+# Secret Labs AB or the author not be used in advertising or publicity 
+# pertaining to distribution of the software without specific, written 
+# prior permission.
+#
+# SECRET LABS AB AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO
+# THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY 
+# AND FITNESS. IN NO EVENT SHALL SECRET LABS AB OR THE AUTHOR BE LIABLE
+# FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES 
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+# OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 def tokenize(text, match=re.compile("([idel])|(\d+):|(-?\d+)").match):
     i = 0
@@ -71,7 +93,8 @@ def decode(text):
         raise SyntaxError("syntax error")
     return data
 
-################
+# end code from http://effbot.org/zone/bencode.htm
+###################################################
 
 names = {
     'fc37:8636:7dea:7f3d:6514:ce73:0f7b:f29d': 'gerard_',
